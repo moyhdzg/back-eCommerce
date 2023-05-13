4bf945283d4b3f55b3ae27f648ad637c4bf46b8b
Moises Hdz <moy.hdz.g@gmail.com>
2023-05-13 14:34:26 -0700

    changing from bcrypt to bcryptjs

--- model/users.js
+++ model/users.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 const {Schema, model} = mongoose
 const uniqueValidator = require('mongoose-unique-validator')
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcryptjs')
 
 const userSchema = new Schema({
     name: {
